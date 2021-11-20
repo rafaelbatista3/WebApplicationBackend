bc9d650e7000e9ec286677bff957118825dc5df7
rafaelbatista3 <84281638+rafaelbatista3@users.noreply.github.com>
2021-11-20 17:13:44 -0300

    New feature

---
+++ WebApplicationBackend/Models/Veiculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationBackend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a placa!")]
+        public string Placa { get; set; }
+    }
+}
